afa19f3442014565adf8a4b280bbcb25fe382c16
JarhadPratibha <88483928+JarhadPratibha@users.noreply.github.com>
2021-10-18 04:24:55 +0530

    Add files via upload

--- StringCalculatorDemo/src/main/java/StringCalculator.java
+++ StringCalculatorDemo/src/main/java/StringCalculator.java
@@ -1,20 +1,73 @@
 
 public class StringCalculator {
 	
+	public String checkNegative(String input1)
+	{
+		char[] input=input1.toCharArray();
+		int flag=0;
+		String str="";
+		for(int j=0;j<input.length;j++)
+		{
+			if(Integer.parseInt(String.valueOf(input[j]))<0)
+			{
+				flag=1;
+				str=str+input[j]+",";
+			}
+		}
+		if(flag==1)
+			return "Numbers should be positive only, "+str;	
+		return "0";
+	}
 	public String add(String numbers)
 	{
+		char[] numbers1=numbers.toCharArray();
+		String str="";
+				
+		//if empty string
 		if(numbers.isEmpty())
 			return "0";
-		else
+		
+		//if string contains single integer
+		if(numbers.length()==1)
+			return ""+numbers;
+		 
+		//to check negative numbers in string
+		//else if(str.equals(checkNegative(numbers)))
+			//return str;
+		
+		else if(numbers1[0]=='/' && numbers1[1]=='/')
+		{//;\n1;2=3 
+			
+			int sum1=0;
+			for(int i=0;i<numbers.length();i++)
+			{	
+				if(numbers.charAt(i)=='/' && numbers.charAt(i++)=='/')
+					i=i+2;
+				if(numbers.charAt(i)==';')
+					i++;
+				//if string contains \n
+				if(numbers.charAt(i)=='\n')
+					i++;
+				sum1=sum1+Character.getNumericValue(numbers.charAt(i));
+			}
+			return ""+sum1;	
+		}
+		
+		else//add the given numbers
 		{
 			int sum=0;
 			for(int i=0;i<numbers.length();i++)
+			{	
+				if(numbers.charAt(i)==',')
+					i++;
+				//if string contains \n
+				if(numbers.charAt(i)=='\n')
+					i++;
 				sum=sum+Character.getNumericValue(numbers.charAt(i));
-			
+			}
 			return ""+sum;
 		}
 		
-		//return "0";
 	}
 
 }
--- StringCalculatorDemo/src/test/java/StringCalculatorShould.java
+++ StringCalculatorDemo/src/test/java/StringCalculatorShould.java
@@ -8,24 +8,59 @@ public class StringCalculatorShould {
 	
 	StringCalculator stringCalculator=new StringCalculator();
 	
+	
 	@Test
 	public void when_input_is_empty_return_0()
 	{
 		String number="";
-		stringCalculator.add(number);
-		assertEquals("",number);
+		assertEquals("0",stringCalculator.add(number));
+	}
+	
+	@Test
+	public void when_input_is_1_number_return_1()
+	{
+		String number="5";
+		assertEquals("5",stringCalculator.add(number));
 	}
 	
 	@Test
 	public void when_input_are__2_5_numbers_return_7()
 	{
-		String number="25";
-		
+		String number="2,5";
 		assertEquals("7",stringCalculator.add(number));
 	}
 	
+	@Test
+	public void when_input_are__unknown_numbers_return_7()
+	{
+		String number="2,5,1,4";
+		assertEquals("12",stringCalculator.add(number));
+	}
 
+	@Test
+	public void when_input_are__1_newline_character_2_5_return_6()
+	{
+		//1+/n2+3=6
+		String number="1,\n2,3";
+		assertEquals("6",stringCalculator.add(number));
+	}
 	
+	@Test
+	public void when_delimiter_is_changed_result_should_be_addition_of_given_numbers()
+	//delimiter is changed to ';' previous delimiter was ','
+	{
+		/*  //;\n1;2=3   */
+		String number="//;\n1;2";
+		assertEquals("3", stringCalculator.add(number));
+	}
 	
-
+	/*
+	@Test
+	//if negative numbers are passed
+	public void when_negative_numbers_passed_to_add_function_throw_exception()
+	{
+		String number="1,2,-1,4,-2";
+		assertEquals("Numbers should be positive only, -1,-2", stringCalculator.add(number));
+	}*/
+	
 }
